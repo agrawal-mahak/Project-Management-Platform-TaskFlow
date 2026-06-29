743d4c17692905b54acf443236692292d0c12275
agrawal-mahak <agrawalmahak7@gmail.com>
2026-06-29 11:54:03 +0530

    delete and getbyid added

--- server/controllers/boardController.ts
+++ server/controllers/boardController.ts
@@ -23,3 +23,32 @@ export const createBoard = async (req: Request, res: Response) => {
     }
 
 }
+
+export const getBoardById = async (req: Request, res:Response) =>{
+    try{
+        const {boardId} = req.params;
+        const board = await Board.findById(boardId);
+        if(!board){
+            return res.status(404).json({message:"Board not found"})
+        }
+        res.status(200).json(board)
+    }catch(error){
+        console.error(error)
+        res.status(500).json({message:"Internal server error"})
+
+    }
+}
+
+export const deleteBoard = async (req: Request, res: Response)=> {
+    try{
+        const {boardId} = req.params;
+        const board = await Board.findByIdAndDelete(boardId);
+        if(!board){
+            return res.status(400).json({message: "Board not found"});
+        }
+        res.status(200).json({message: "Board deleted successfully"})
+    } catch (error){
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
