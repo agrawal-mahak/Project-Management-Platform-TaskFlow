cfc4255d58a6aff28b65314104014e05d6f5340c
agrawal-mahak <agrawalmahak7@gmail.com>
2026-07-20 19:48:07 +0530

    url chnged

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
+    // proxy: {
+    //   "/api": "http://localhost:5000",
+    //   "/uploads": "http://localhost:5000",
+    // }
     proxy: {
-      "/api": "http://localhost:5000",
-      "/uploads": "http://localhost:5000",
+      "/api": "https://project-management-platform-taskflow.onrender.com",
+      "/uploads": "https://project-management-platform-taskflow.onrender.com",
     }
   }
 });
